56629363ab36e44b66a2e8ccbf8cc3f109d90e4f
anraku <anraku90@gmail.com>
2016-12-18 21:48:46 +0900

    go web sample

---
+++ chat_profile/client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"github.com/gorilla/websocket"
+	"time"
+)
+
+// clientはチャットを行っている1人のユーザを表しています。
+type client struct {
+	// socketはこのクライアントのためのWebSocketです。
+	socket *websocket.Conn
+	// sendはメッセージが送られるチャネルです。
+	send chan *message
+	// room はこのクライアントが参加しているチャットルームです。
+	room *room
+	// userDataはユーザーに関する情報を保持します
+	userData map[string]interface{}
+}
+
+func (c *client) read() {
+	for {
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
+			//msg.AvatarURL, _ = c.room.avatar.GetAvatarURL(c)
+			if avatarURL, ok := c.userData["avatar_url"]; ok {
+				msg.AvatarURL = avatarURL.(string)
+			}
+			c.room.forward <- msg
+		} else {
+			break
+		}
+	}
+	c.socket.Close()
+}
+
+func (c *client) write() {
+	for msg := range c.send {
+		if err := c.socket.WriteJSON(msg); err != nil {
+			break
+		}
+	}
+	c.socket.Close()
+}
---
+++ chat_profile/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"../chat_room/trace"
+	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+// 現在アクティブなAvatarの実装
+var avatars Avatar = TryAvatars{
+	UseFileSystemAvatar,
+	UseAuthAvatar,
+	UseGravatar}
+
+// templは1つのテンプレートを表します
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServerHTTPはHTTPリクエストを処理します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() // フラグを解釈します
+	//Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("1111")
+	gomniauth.WithProviders(
+		google.New("827778022927-5rh6jo8mauis03rr7t682ejnkpadqfl7.apps.googleusercontent.com", "PwQKcyBaCGo5biCsAqCfUfK4", "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHandler)
+	http.Handle("/room", r)
+	http.Handle("/upload", &templateHandler{filename: "upload.html"})
+	http.HandleFunc("/uploader", uploaderHandler)
+	http.Handle("/avatars/",
+		http.StripPrefix("/avatars/", http.FileServer(http.Dir("./avatars/"))))
+	http.HandleFunc("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:   "auth",
+			Value:  "",
+			Path:   "/",
+			MaxAge: -1,
+		})
+		w.Header()["Location"] = []string{"/chat"}
+		w.WriteHeader(http.StatusTemporaryRedirect)
+	})
+	// チャットルームを開始します。
+	go r.run()
+	// Webサーバを起動します。
+	log.Println("Webサーバを開始します。ポート： ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
